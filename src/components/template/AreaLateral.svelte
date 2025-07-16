06af6d7bd761d05b547f123f7dfdd006ac52f7b6
julio <juliocesar.melegari@gmail.com>
2025-07-15 22:01:47 -0300

    add areaLateral

---
+++ src/components/template/AreaLateral.svelte
@@ -0,0 +1,9 @@
+<aside class="
+    flex flex-col self-start
+    border border-zinc-800 p-5 rounded-md
+    gap-10 w-72"
+>
+    <span>Menu #1</span>
+    <span>Menu #2</span>
+    <span>Menu #3</span>
+</aside>
